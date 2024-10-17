a82bba61408fbd0ec5f923a0b23c5f81417abea3
Cihan <officialcihan0248@gmail.com>
2024-10-17 16:13:54 +0300

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,9 +4,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='max-w-3xl text-gray-400 mx-auto pt-10 px-4 absolute bottom-4 left-1/2 transform -translate-x-1/2 flex items-center justify-center gap-2 text-sm'>
-        <Link href={"/"} className='hover:text-blue-700 transition-all duration-200 ease-in-out'>twitter</Link>
-        <Link href={"/"} className='hover:text-blue-700 transition-all duration-200 ease-in-out'>github</Link>
-        <Link href={"/"} className='hover:text-blue-700 transition-all duration-200 ease-in-out'>linkedin</Link>
+        <Link href={"https://github.com/cihanisildar"} className='hover:text-blue-700 transition-all duration-200 ease-in-out'>github</Link>
+        <Link href={"https://www.linkedin.com/in/cihanisildar/"} className='hover:text-blue-700 transition-all duration-200 ease-in-out'>linkedin</Link>
     </div>
   )
 }
